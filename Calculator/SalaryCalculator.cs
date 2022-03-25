427564162b1c83c5e4d34d28052937d917ad6e86
Ash Singh <ash.singh@truelayer.com>
2022-03-25 19:10:06 +0000

    Xunit demonstrations

---
+++ Calculator/SalaryCalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculator
+{
+    public class SalaryCalculator
+    {
+        const int hoursInYear = 2080;
+        public decimal GetAnnualSalary(decimal hourlyWage)
+        {
+            return hourlyWage * hoursInYear;
+        }
+
+        public decimal GetHourlyWage(decimal yearlyWage)
+        {   
+            return yearlyWage / hoursInYear;
+        }
+    }
+}
---
+++ SalaryCalculatorTestProject/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Calculator;
+using NUnit.Framework;
+
+namespace SalaryCalculatorTestProject
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AnnualSalaryTest()
+        {
+            // Arrange
+            SalaryCalculator sc = new SalaryCalculator();
+            
+            // Act
+            decimal annualSalary = sc.GetAnnualSalary(50);
+            
+            // Assert
+            Assert.AreEqual(104000, annualSalary);
+        }
+        
+        [Test]
+        public void HourlyWageTest()
+        {
+            // Arrange
+            SalaryCalculator sc = new SalaryCalculator();
+            
+            // Act
+            decimal hourlyWage = sc.GetHourlyWage(104000);
+            
+            // Assert
+            Assert.AreEqual(50m, hourlyWage);
+        }
+    }
+}
